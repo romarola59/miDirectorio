52c1756fd27544bc996598e387818415149e04c0
Rosa M. Rodriguez <romarola@hotmail.com>
2018-09-23 20:37:29 -0300

    Agregar archivos de proyecto.

---
+++ midirectorio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace midirectorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
